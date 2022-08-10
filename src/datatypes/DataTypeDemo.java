9835b736c5c15b13a1e54c5c88b0afa84abbe9d2
Venkatesh <venkateshbelgaonkar777@gmail.com>
2022-08-10 23:21:16 +0530

    Updated

---
+++ src/datatypes/DataTypeDemo.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+
+        /* Max and Min Integer value */
+        int maxIntValue = Integer.MAX_VALUE;
+        int minIntValue = Integer.MIN_VALUE;
+        System.out.println("Maximum Integer value : " + maxIntValue);
+        System.out.println("Minimum Integer value : " + minIntValue);
+
+        /* Max and Min float value */
+        float maxFloatValue = Float.MAX_VALUE;
+        float minFloatValue = Float.MIN_VALUE;
+        System.out.println("Maximum Float value : " + maxFloatValue);
+        System.out.println("Minimum Float value : " + minFloatValue);
+
+    }
+}
