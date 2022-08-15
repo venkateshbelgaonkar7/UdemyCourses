f91a0780c2683c957fddefd1513ab6a85be75b0e
Venkatesh <venkateshbelgaonkar777@gmail.com>
2022-08-15 13:59:31 +0530

    String Examples

---
+++ src/datatypes/StringDemo.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+    }
+}
